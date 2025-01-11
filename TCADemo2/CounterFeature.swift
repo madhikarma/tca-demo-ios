89bad1230f681bdb167fd6636bf683eb91a3d9ad
Shagun Madhikarmi <madhikarma@gmail.com>
2025-01-11 06:28:44 +0000

    Cleanup Dog Breeds API code from default Counter feature

--- TCADemo2/CounterFeature.swift
+++ TCADemo2/CounterFeature.swift
@@ -12,47 +12,25 @@ struct CounterFeature {
   @ObservableState
   struct State {
     var count = 0
-    var response: Result<[DogBreed], Error>?
   }
   
   enum Action {
     case decrementButtonTapped
     case incrementButtonTapped
-    case fetchData
-    case fetchResponse(Result<[DogBreed], Error>)
   }
   
-  var body: some ReducerOf<Self> {
-    Reduce { state, action in
-      switch action {
-      case .decrementButtonTapped:
-        state.count -= 1
-          
-        return .none
-        
-      case .incrementButtonTapped:
-        state.count += 1
-        
-        return .none
-          
-    // API / Network example taken from https://medium.com/@gangadharganga90/how-to-make-api-calls-in-swiftui-with-tca-framework-80f0980afa47
-      case .fetchData:
-          state.response = nil
-          return .run { [state = state] send in
-              let dogBreeds = try await DogsAPI().fetchDogBreeds()
-              print(dogBreeds.debugDescription)
-              await send(.fetchResponse(.success(dogBreeds)))
-          }
-      case .fetchResponse(.success(let data)):
-          state.response = .success(data)
-          return .none
-          
-      case .fetchResponse(.failure(let error)):
-          state.response = .failure(error)
-//          state.errorMessage = error.rawValue
-          return .none
-
-      }
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            case .decrementButtonTapped:
+                state.count -= 1
+                
+                return .none
+                
+            case .incrementButtonTapped:
+                state.count += 1
+                return .none
+            }
+        }
     }
-  }
 }
